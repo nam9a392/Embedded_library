40cf2e51523f19c1a87d2182ec71eb8cdb2f6e56
DESKTOP-UKLRGIL\NCPT <nam9a392@gmail.com>
2025-09-08 14:41:32 +0700

    update code

--- FRAM/fm25l16b/fm25l16b.c
+++ FRAM/fm25l16b/fm25l16b.c
@@ -19,13 +19,13 @@
 *                                              ENUMS
 ==================================================================================================*/
 typedef enum{
-	WREN  = 0x06,
-	WRDI  = 0x04,
-	RDSR  = 0x05,
-	WRSR  = 0x01,
-	READ  = 0x03,
-	WRITE = 0x02
-}fm25l16b_cmd_e;
+	OP_WREN  = 0x06,  /* Write Enable Latch */
+	OP_WRDI  = 0x04,  /* Reset Write Enable Latch */
+	OP_RDSR  = 0x05,  /* Read Status Register */
+	OP_WRSR  = 0x01,  /* Write Status Register */
+	OP_READ  = 0x03,  /* Read Memory */
+	OP_WRITE = 0x02   /* Write Memory */
+}fm25l16b_opcode_e;
 /*==================================================================================================
 *                                  STRUCTURES AND OTHER TYPEDEFS
 ==================================================================================================*/
@@ -52,5 +52,97 @@ typedef enum{
 
 void FM25L16B_Init(fm25l16b_t *me, fm25l16bIF_t *meIF)
 {
-	me->fm25l16bIF = meIF;
+	me->meIF = meIF;
+}
+
+void FM25L16B_write_enable(fm25l16b_t *me , bool enable)
+{
+	fm25l16bIF_t *tmpIF = me.meIF;
+	tmpIF->wait_busy();
+	if(true == enable){
+		me->cmd[0] = OP_WREN;
+	}else{
+		me->cmd[0] = OP_WRDI;
+	}
+	
+	tmpIF->enable();
+	tmpIF->spi_write(cmd,1);
+	tmpIF->disable();
+}
+
+bool FM25L16B_get_status(fm25l16b_t *me, fm25l16b_status_t *status)
+{
+	bool retVal = true;
+	uint8_t i = 0;
+	uint8_t status_reg = 0;
+	fm25l16bIF_t *tmpIF = me.meIF;
+	cmd[i++] = OP_RDSR;
+
+	tmpIF->enable();
+	tmpIF->spi_write(cmd,i);
+	tmpIF->wait_busy();
+	tmpIF->spi_read(&status_reg,1);
+	tmpIF->wait_busy();
+	tmpIF->disable();
+	
+	status->WEL  = (status_reg >> 0x01) & 0x01;
+	status->BP0  = (status_reg >> 0x02) & 0x01;
+	status->BP1  = (status_reg >> 0x03) & 0x01;
+	status->WPEN = (status_reg >> 0x07) & 0x01;
+	return retVal;
+}
+
+bool FM25L16B_set_status(fm25l16b_t *me, fm25l16b_status_t *status)
+{
+	bool retVal = true;
+	uint8_t i = 0;
+	uint8_t status_reg = 0;
+	status_reg = (status->WEL & 0x01 << 0x01) | (status->BP0 & 0x01 << 0x02) | (status->BP1 & 0x01 << 0x03) | (status->WPEN & 0x01 << 0x07);
+	fm25l16bIF_t *tmpIF = me.meIF;
+	cmd[i++] = OP_WRSR;
+	cmd[i++] = status_reg;
+
+	tmpIF->enable();
+	tmpIF->spi_write(cmd,i);
+	tmpIF->wait_busy();
+	tmpIF->disable();
+	return retVal;
+}
+
+bool FM25L16B_write(fm25l16b_t *me, uint16_t address, uint8_t *buffer, uint32_t numByte)
+{
+	bool retVal = true;
+	uint8_t i = 0;
+	fm25l16bIF_t *tmpIF = me.meIF;
+	cmd[i++] = OP_WRITE;
+	cmd[i++] = (uint8_t)(address >> 8);
+	cmd[i++] = (uint8_t)(address & 0xFF);
+
+	tmpIF->enable();
+	tmpIF->spi_write(cmd,i);
+	tmpIF->wait_busy();
+	tmpIF->spi_write(buffer,numByte);
+	tmpIF->wait_busy();
+	tmpIF->disable();
+
+	return retVal;
+}
+
+bool FM25L16B_read(fm25l16b_t *me, uint16_t address, uint8_t *buffer, uint32_t numByte)
+{
+	bool retVal = true;
+	uint8_t i = 0;
+	fm25l16bIF_t *tmpIF = me.meIF;
+	cmd[i++] = OP_READ;
+	cmd[i++] = (uint8_t)(address >> 8);
+	cmd[i++] = (uint8_t)(address & 0xFF);
+
+	tmpIF->enable();
+	tmpIF->spi_write(cmd,i);
+	tmpIF->wait_busy();
+	tmpIF->spi_read(buffer,numByte);
+	tmpIF->wait_busy();
+	tmpIF->disable();
+
+	return retVal;
 }
--- FRAM/fm25l16b/fm25l16b.h
+++ FRAM/fm25l16b/fm25l16b.h
@@ -32,15 +32,23 @@
 *                                  STRUCTURES AND OTHER TYPEDEFS
 ==================================================================================================*/
 typedef struct{
+	bool WEL;  // Write Enable Latch
+	bool BP0;  // Block Protect bit ‘0’
+	bool BP1;  // Block Protect bit ‘1’
+	bool WPEN; // Write Protect Enable bit
+}fm25l16b_status_t;
+
+typedef struct{
 	void (*enable)(void);
 	void (*disable)(void);
+	void (*spi_read)(uint8_t *pdata,uint16_t len);
 	void (*spi_write)(uint8_t *pdata,uint16_t len);
 	void (*wait_busy)(void);
 }fm25l16bIF_t;
 
 typedef struct{
 	uint8_t      cmd[10];
-	fm25l16bIF_t *fm25l16bIF;
+	fm25l16bIF_t *meIF;
 }fm25l16b_t;
 /*==================================================================================================
 *                                  GLOBAL VARIABLE DECLARATIONS
@@ -50,7 +58,10 @@ typedef struct{
 *                                       FUNCTION PROTOTYPES
 ==================================================================================================*/
 void FM25L16B_Init(fm25l16b_t *me, fm25l16bIF_t *meIF);
-void FM25L16B_get_status(fm25l16b_t *me);
-void FM25L16B_read(fm25l16b_t *me, uint32_t address, uint8_t *buffer, uint32_t numByte);
-void FM25L16B_write(fm25l16b_t *me, uint32_t address, uint8_t *buffer, uint32_t numByte);
+void FM25L16B_write_enable(fm25l16b_t *me);
+bool FM25L16B_get_status(fm25l16b_t *me, fm25l16b_status_t *status);
+bool FM25L16B_set_status(fm25l16b_t *me, fm25l16b_status_t *status);
+void FM25L16B_write(fm25l16b_t *me, uint16_t address, uint8_t *buffer, uint32_t numByte);
+void FM25L16B_read(fm25l16b_t *me, uint16_t address, uint8_t *buffer, uint32_t numByte);
+
 #endif /* USER_DRIVERS_FM25L16B_H_ */
